030e9597fb27d9fbf265c7f867b227b3b893ff84
Por Seavhong <mrhong563.gmail.com>
2025-05-26 10:59:45 +0700

    edit react-router

--- src/RootLaout/RootLayOut.jsx
+++ src/RootLaout/RootLayOut.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router"
 import TopNavbar from "../Components/TopNavbar"
 import Navbar from "../Components/Navbar"
 import Footer from "../Components/Footer"
